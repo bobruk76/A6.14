706daf2c59356cde68ecaf93f9c66fd73d19ba77
Vladimir Poliakov <60501887+bobruk76@users.noreply.github.com>
2020-03-09 21:04:56 +0300

    red boxes added

--- js/game.js
+++ js/game.js
@@ -10,6 +10,10 @@ function round() {
    .empty();
 
   let divSelector = randomDivId();
+  let missSelector = randomDivId();
+
+  $(missSelector).addClass("miss");
+  
   $(divSelector)
     .addClass("target")
     .text(hits+1);
@@ -22,15 +26,19 @@ function round() {
     endGame();
   }
 }
+
 function reloadGame() {
   hits = 0;
   $(".grid-wrapper").removeClass("d-none");
   $("#win-message").addClass("d-none");
+  initProgress();
   round();
 };
 
 function endGame() {
+
   $(".grid-wrapper").addClass("d-none");
+  $("#progressbar").addClass("d-none");
   let totalPlayedMillis = getTimestamp() - firstHitTime;
   let totalPlayedSeconds = Number(totalPlayedMillis / 1000).toPrecision(3);
   $("#total-time-played").text(totalPlayedSeconds);
@@ -39,18 +47,27 @@ function endGame() {
 
 function handleClick(event) {
   if ($(event.target).hasClass("target")) {
+    addProgress();
     $(".miss").removeClass("miss");
     hits = hits + 1;
-    addProgress();
+   
     round();
-  } else {
-    $(event.target).addClass("miss");
-  }
+  } else { if($(event.target).hasClass("miss")) {
+
+  }}
 
 }
-function addProgress() {
+
+function initProgress() {
+  $("#progressbar")
+      .attr("aria-valuenow",0)
+      .css("width", "0%")
+      .text("")
+      .removeClass("d-none");
+};
+
+function addProgress(procent = 100 / maxHits) {
   $("#progressbar").each(function() {
-    let procent = 100 / maxHits;
     let current_progress = procent + parseInt($(this).attr("aria-valuenow"));
     $(this)
       .attr("aria-valuenow",current_progress)
